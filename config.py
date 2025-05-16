7f43d2d0b1e514d3d38823e5c487d4914c05e06c
SalmonSung <tuebingen0526@gmail.com>
2025-05-16 09:57:24 +0200

    v0.1 structure building

---
+++ config.py
@@ -0,0 +1,4 @@
+import os
+APP_PATH_ROOT = os.path.dirname(os.path.abspath(__file__))
+APP_PATH_DATASET = os.path.join(APP_PATH_ROOT, 'dataset')
+APP_PATH_RESULT = os.path.join(APP_PATH_ROOT, 'result')
